5bb3b16c680f6078353bb98cd490ac183b4b0751
rkozhan~ <yalta.vrk@gmail.com>
2024-08-07 20:17:41 +0200

    migrating to Minimal APIs

--- WebApplicationApi/Program.cs
+++ WebApplicationApi/Program.cs
@@ -54,6 +54,58 @@ app.MapGet("products/{id}", async (int id, ShopContext _context) =>
         return Results.NotFound();
     }
     return Results.Ok(product);
+}).WithName("GetProduct");
+
+app.MapPost("/products/", async (ShopContext _context, Product product) => 
+{
+    _context.Products.Add(product);
+    await _context.SaveChangesAsync();
+
+    return Results.CreatedAtRoute(
+        "GetProduct",
+        new { id = product.Id },
+        product);
+});
+
+app.MapPut("/products/{id}", async (ShopContext _context, int id, Product product) =>
+{
+    if (id != product.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    _context.Entry(product).State = EntityState.Modified;
+
+    try
+    {
+        await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!_context.Products.Any(p => p.Id == id))
+        {
+            return Results.NotFound();
+        } else
+        {
+            throw;
+        }
+    }
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/products/{id}", async (ShopContext _context, int id) =>
+{
+    var product = await _context.Products.FindAsync(id);
+    if (product == null)
+    {
+        return Results.NotFound();
+    }
+
+    _context.Products.Remove(product);
+    await _context.SaveChangesAsync();
+
+    return Results.Ok(product);
 });
 
 app.Run();
